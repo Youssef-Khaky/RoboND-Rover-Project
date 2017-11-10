71b8baa53b06ad2b5f4e12971005aadfe7761d49
youssefkhaky <youssefkhaky@hotmail.com>
2017-11-10 18:25:25 -0500

    final commit

--- code/decision.py
+++ code/decision.py
@@ -11,9 +11,11 @@ def decision_step(Rover):
 
     # Example:
     # Check if we have vision data to make decisions with
+
     if Rover.nav_angles is not None:
         # Check for Rover.mode status
         if Rover.mode == 'forward':
+            print ("forward")
             # Check the extent of navigable terrain
             if len(Rover.nav_angles) >= Rover.stop_forward:
                 # If mode is forward, navigable terrain looks good
@@ -25,8 +27,8 @@ def decision_step(Rover):
                     Rover.throttle = 0
                 Rover.brake = 0
                 # Set steering to average angle clipped to the range +/- 15
-                Rover.steer = np.clip(np.mean(Rover.nav_angles * 180/np.pi), -15, 15)
-                print(np.mean(Rover.nav_angles* 180/np.pi)," ",np.max(Rover.nav_angles* 180/np.pi))
+                Rover.steer = np.clip(np.mean(Rover.nav_angles * 180/np.pi) + 17, -10, 10)
+                print(np.mean(Rover.nav_angles* 180/np.pi)," ",np.mean(Rover.nav_angles* 180/np.pi)+17)
             # If there's a lack of navigable terrain pixels then go to 'stop' mode
             elif len(Rover.nav_angles) < Rover.stop_forward:
                     # Set mode to "stop" and hit the brakes!
@@ -38,6 +40,7 @@ def decision_step(Rover):
 
         # If we're already in "stop" mode then make different decisions
         elif Rover.mode == 'stop':
+            print("stopped")
             # If we're in stop mode but still moving keep braking
             if Rover.vel > 0.2:
                 Rover.throttle = 0
@@ -51,6 +54,9 @@ def decision_step(Rover):
                     # Release the brake to allow turning
                     Rover.brake = 0
                     # Turn range is +/- 15 degrees, when stopped the next line will induce 4-wheel turning
+                    # try:
+                    #     Rover.steer = np.clip(np.mean(Rover.nav_angles * 180/np.pi) - 15, -15, 15)
+                    # except:
                     Rover.steer = -15 # Could be more clever here about which way to turn
                 # If we're stopped but see sufficient navigable terrain in front then go!
                 if len(Rover.nav_angles) >= Rover.go_forward:
--- code/perception.py
+++ code/perception.py
@@ -1,5 +1,7 @@
 import numpy as np
 import cv2
+import matplotlib.pyplot as plt
+import time
 
 # Identify pixels above the threshold
 # Threshold of RGB > 160 does a nice job of identifying ground pixels only
@@ -8,6 +10,18 @@ import cv2
 def color_thresh_nav(img, rgb_thresh=(160, 160, 160)):#lower thresh
     # Create an array of zeros same xy size as img, but single channel
     color_select = np.zeros_like(img[:,:,0])
+    
+    mask = np.zeros_like(img[:,:,0])
+    #mask[int(mask.shape[0]/2):mask.shape[0]-1,int(mask.shape[1]/2):mask.shape[1]-1] = 1
+    mask[int(mask.shape[0]/2):mask.shape[0]-1,:] = 1
+    
+        	
+
+
+    
+    
+    
+    
     # Require that each pixel be above all three threshold values in RGB
     # above_thresh will now contain a boolean array with "True"
     # where threshold was met
@@ -16,6 +30,8 @@ def color_thresh_nav(img, rgb_thresh=(160, 160, 160)):#lower thresh
                 & (img[:,:,2] > rgb_thresh[2])
     # Index the array of zeros with the boolean array and set to 1
     color_select[navigable] = 1
+    
+    color_select = cv2.bitwise_and(color_select,color_select,mask = mask)
 
     # Return the binary image
     return color_select
@@ -110,7 +126,6 @@ def perspect_transform(img, src, dst):
 
 # Apply the above functions in succession and update the Rover state accordingly
 def perception_step(Rover):
-
     image = Rover.img
 
     xpos = Rover.pos[0]
@@ -126,17 +141,19 @@ def perception_step(Rover):
                       [image.shape[1]/2 - dst_size, image.shape[0] - 2*dst_size - bottom_offset],
                       ])
 
-
+    
     # 2) Apply perspective transform
     warped = perspect_transform(image, source, destination)
     # 3) Apply color threshold to identify navigable terrain/obstacles/rock samples
     navigable = color_thresh_nav(warped) #bin image of eagle eye
+    
+    
     rock = color_thresh_rock(warped)
     obs = color_thresh_obs(warped)
 
     # 4) Update Rover.vision_image (this will be displayed on left side of screen)
-    Rover.vision_image[:,:,0] = obs*255 #obstacle color-thresholded binary image
-    Rover.vision_image[:,:,1] = rock*255 #rock_sample color-thresholded binary image
+    # Rover.vision_image[:,:,0] = obs*255 #obstacle color-thresholded binary image
+    # Rover.vision_image[:,:,1] = rock*255 #rock_sample color-thresholded binary image
     Rover.vision_image[:,:,2] = navigable*255 #navigable terrain color-thresholded binary image
 
     # 5) Convert map image pixel values to rover-centric coords
@@ -149,12 +166,12 @@ def perception_step(Rover):
     rock_world_x, rock_world_y = pix_to_world(rock_rover_x, rock_rover_y, xpos, ypos, yaw, 200, 10)
     obs_world_x, obs_world_y = pix_to_world(obs_rover_x, obs_rover_y, xpos, ypos, yaw, 200, 10)
 
-    Rover.worldmap[obs_world_y, obs_world_x, 0] += 1
-    Rover.worldmap[rock_world_y, rock_world_x, :] = 255
+    # Rover.worldmap[obs_world_y, obs_world_x, 0] += 1
+    # Rover.worldmap[rock_world_y, rock_world_x, :] = 255
     Rover.worldmap[nav_world_y, nav_world_x, 2] += 10
 
 
-    dist, angle = to_polar_coords(nav_world_x, nav_world_y)
+    dist, angle = to_polar_coords(nav_rover_x, nav_rover_y)
 
 
     # 7) Update Rover worldmap (to be displayed on right side of screen)
@@ -163,6 +180,38 @@ def perception_step(Rover):
     # Update Rover pixel distances and angles
     Rover.nav_dists = dist
     Rover.nav_angles = angle
+#    mean_dir = np.mean(angle)
+    
+    
+#    threshed = color_thresh_nav(warped)
+    
+    # Calculate pixel values in rover-centric coords and distance/angle to all pixels
+#    xpix, ypix = rover_coords(threshed)
+#    dist, angles = to_polar_coords(xpix, ypix)
+#    mean_dir = np.mean(angles)
+    # Do some plotting
+#    fig = plt.figure(figsize=(12,12))
+#    plt.subplot(221)
+#    plt.imshow(image)
+#    plt.subplot(222)
+#    plt.imshow(warped)
+#    plt.subplot(223)
+#    plt.imshow(navigable, cmap='gray')
+#    plt.subplot(224)
+#    plt.plot(nav_rover_x, nav_rover_y, '.')
+#    plt.ylim(-160, 160)
+#    plt.xlim(0, 160)
+#    arrow_length = 100
+#    x_arrow = arrow_length * np.cos(mean_dir)
+#    y_arrow = arrow_length * np.sin(mean_dir)
+#    plt.arrow(0, 0, x_arrow, y_arrow, color='red', zorder=2, head_width=10, width=2)
+#    plt.ion()
+#    
+#    plt.show()
+#    plt.pause(0.0001)
+#    plt.close()
+#
+#
 
 
 
